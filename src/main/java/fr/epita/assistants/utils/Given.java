601898a28dd0f933596ef26a5c66896991a59254
jeremy.d-auria <jeremy.d-auria@epita.fr>
2021-05-30 23:57:09 +0200

    chore(given): add given files

---
+++ src/main/java/fr/epita/assistants/utils/Given.java
@@ -0,0 +1,17 @@
+package fr.epita.assistants.utils;
+
+/**
+ * All classes with this annotation are provided by the assistants.
+ */
+public @interface Given {
+
+    /**
+     * Will this file be overridden at compilation time by the moulinette?
+     * Note: changing this field locally will not change the moulinette's
+     * behavior, this is an indication of intent, not something you can
+     * unilaterally change.
+     *
+     * @return `true` if the file will be overridden, false otherwise.
+     */
+    boolean overridden() default true;
+}
