db2f7e8466787056931815a0f554a0cffc0827ed
Tony HENG <tony.heng@epita.fr>
2021-06-07 23:55:35 +0200

    add(feature/git): begin Push&Pull Features, TODO: add tests

--- src/main/java/fr/epita/assistants/features/git/PullFeature.java
+++ src/main/java/fr/epita/assistants/features/git/PullFeature.java
@@ -1,16 +1,37 @@
 package fr.epita.assistants.features.git;
 
 import fr.epita.assistants.myide.domain.entity.Feature;
+import fr.epita.assistants.myide.domain.entity.Mandatory;
 import fr.epita.assistants.myide.domain.entity.Project;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+
+import java.io.IOException;
 
 public class PullFeature implements Feature {
     @Override
     public ExecutionReport execute(Project project, Object... params) {
-        return null;
+        Git git = null;
+        try {
+            git = Git.open(project.getRootNode().getPath().toFile());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return () -> false;
+        }
+
+        //TODO: pull in other branches (not sure if required)
+        try {
+            git.pull().call();
+        } catch (GitAPIException e) {
+            e.printStackTrace();
+            return () -> false;
+        }
+        return () -> true;
+
     }
 
     @Override
     public Type type() {
-        return null;
+        return Mandatory.Features.Git.PULL;
     }
 }
---
+++ src/test/java/PushFeatureTest.java
@@ -0,0 +1,84 @@
+import fr.epita.assistants.MyProjectService;
+import fr.epita.assistants.features.git.PushFeature;
+import fr.epita.assistants.myide.domain.entity.Project;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class PushFeatureTest {
+    private Path path;
+    private File file;
+    private Git git;
+    private PushFeature feature;
+
+    @BeforeEach
+    void setUp() throws GitAPIException {
+        file = new File("src/test/testFiles/tmp");
+        path = file.toPath();
+        file.mkdirs();
+        git = Git.init().setDirectory(file).call();
+        feature = new PushFeature();
+    }
+
+    @AfterEach
+    void tearDown() {
+        deleteDir(file);
+    }
+
+    public void deleteDir(File dir) {
+        File[] files = dir.listFiles();
+        if (files != null) {
+            for (final File file : files)
+                deleteDir(file);
+        }
+        dir.delete();
+    }
+
+    @Test
+    public void pushOne() throws GitAPIException {
+        File subfile1 = new File("src/test/testFiles/tmp/file1");
+        File subfile2 = new File("src/test/testFiles/tmp/file2");
+        try {
+            subfile1.createNewFile();
+            subfile2.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Assertions.fail();
+            return;
+        }
+
+        MyProjectService projectService = new MyProjectService();
+        Project project = projectService.load(file.toPath());
+
+        git.add().addFilepattern("file1").call();
+        git.commit().setMessage("message").call();
+        Assertions.assertTrue(feature.execute(project).isSuccess());
+    }
+
+    @Test
+    public void pushNoCommit() throws GitAPIException {
+        File subfile1 = new File("src/test/testFiles/tmp/file1");
+        File subfile2 = new File("src/test/testFiles/tmp/file2");
+        try {
+            subfile1.createNewFile();
+            subfile2.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Assertions.fail();
+            return;
+        }
+
+        MyProjectService projectService = new MyProjectService();
+        Project project = projectService.load(file.toPath());
+
+        git.add().addFilepattern("file1").call();
+        Assertions.assertFalse(feature.execute(project).isSuccess());
+    }
+}
