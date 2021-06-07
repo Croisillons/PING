c9ebb8a55351bfbab629ed15e007c07c0a50eadc
Tony HENG <tony.heng@epita.fr>
2021-06-07 23:07:25 +0200

    add(feature/git): add AddFeature

--- src/main/java/fr/epita/assistants/features/git/AddFeature.java
+++ src/main/java/fr/epita/assistants/features/git/AddFeature.java
@@ -1,16 +1,43 @@
 package fr.epita.assistants.features.git;
 
 import fr.epita.assistants.myide.domain.entity.Feature;
+import fr.epita.assistants.myide.domain.entity.Mandatory;
 import fr.epita.assistants.myide.domain.entity.Project;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+
+import java.io.IOException;
+import java.nio.file.Path;
 
 public class AddFeature implements Feature {
     @Override
     public ExecutionReport execute(Project project, Object... params) {
-        return null;
+        Git git = null;
+        try {
+            git = Git.open(project.getRootNode().getPath().toFile());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return () -> false;
+        }
+
+        String rootPath = project.getRootNode().getPath().toString();
+        for (Object param : params) {
+            try {
+                Path path = Path.of(rootPath + '/' + param);
+                if (!path.toFile().exists())
+                    return () -> false;
+                git.add().addFilepattern((String) param).call();
+            } catch (GitAPIException e) {
+                e.printStackTrace();
+                return () -> false;
+            }
+        }
+
+        return () -> true;
     }
 
     @Override
     public Type type() {
-        return null;
+        return Mandatory.Features.Git.ADD;
     }
 }
---
+++ src/test/java/AddFeatureTest.java
@@ -0,0 +1,134 @@
+import fr.epita.assistants.MyProjectService;
+import fr.epita.assistants.features.any.CleanupFeature;
+import fr.epita.assistants.features.git.AddFeature;
+import fr.epita.assistants.myide.domain.entity.Project;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.Status;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Set;
+
+public class AddFeatureTest {
+    private Path path;
+    private File file;
+    private Git git;
+    private AddFeature feature;
+
+    @BeforeEach
+    void setUp() throws GitAPIException {
+        file = new File("src/test/testFiles/tmp");
+        path = file.toPath();
+        file.mkdirs();
+        git = Git.init().setDirectory(file).call();
+        feature = new AddFeature();
+    }
+
+    @AfterEach
+    void tearDown() {
+        deleteDir(file);
+    }
+
+    public void deleteDir(File dir) {
+        File[] files = dir.listFiles();
+        if (files != null) {
+            for (final File file : files)
+                deleteDir(file);
+        }
+        dir.delete();
+    }
+
+    @Test
+    public void addOne() throws GitAPIException {
+        File subfile1 = new File("src/test/testFiles/tmp/file1");
+        File subfile2 = new File("src/test/testFiles/tmp/file2");
+        try {
+            subfile1.createNewFile();
+            subfile2.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Assertions.fail();
+            return;
+        }
+
+        MyProjectService projectService = new MyProjectService();
+        Project project = projectService.load(file.toPath());
+
+        Assertions.assertTrue(feature.execute(project, "file1").isSuccess());
+        Status status = git.status().call();
+        Set<String> added = status.getAdded();
+        Assertions.assertEquals(1, added.size());
+    }
+
+    @Test
+    public void addTwo() throws GitAPIException {
+        File subfile1 = new File("src/test/testFiles/tmp/file1");
+        File subfile2 = new File("src/test/testFiles/tmp/file2");
+        try {
+            subfile1.createNewFile();
+            subfile2.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Assertions.fail();
+            return;
+        }
+
+        MyProjectService projectService = new MyProjectService();
+        Project project = projectService.load(file.toPath());
+
+        Assertions.assertTrue(feature.execute(project, "file1", "file2").isSuccess());
+        Status status = git.status().call();
+        Set<String> added = status.getAdded();
+        Assertions.assertEquals(2, added.size());
+    }
+
+    @Test
+    public void addFailed() throws GitAPIException {
+        File subfile1 = new File("src/test/testFiles/tmp/file1");
+        File subfile2 = new File("src/test/testFiles/tmp/file2");
+        try {
+            subfile1.createNewFile();
+            subfile2.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Assertions.fail();
+            return;
+        }
+
+        MyProjectService projectService = new MyProjectService();
+        Project project = projectService.load(file.toPath());
+
+        Assertions.assertFalse(feature.execute(project, "file3").isSuccess());
+        Status status = git.status().call();
+        Set<String> added = status.getAdded();
+        Assertions.assertEquals(0, added.size());
+    }
+
+//    @Test
+//    public void addRegex() throws GitAPIException {
+//        File subfile1 = new File("src/test/testFiles/tmp/file1");
+//        File subfile2 = new File("src/test/testFiles/tmp/file2");
+//        try {
+//            subfile1.createNewFile();
+//            subfile2.createNewFile();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            Assertions.fail();
+//            return;
+//        }
+//
+//        MyProjectService projectService = new MyProjectService();
+//        Project project = projectService.load(file.toPath());
+//
+//        Assertions.assertTrue(feature.execute(project, ".").isSuccess());
+//        Status status = git.status().call();
+//        Set<String> added = status.getAdded();
+//        Assertions.assertEquals(2, added.size());
+//    }
+}
