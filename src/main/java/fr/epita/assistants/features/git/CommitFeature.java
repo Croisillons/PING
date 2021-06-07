a2da533cc6e993ea1c8a0466c11f0321594bca40
Tony HENG <tony.heng@epita.fr>
2021-06-07 16:44:12 +0200

    add(feature/git): add files

---
+++ src/main/java/fr/epita/assistants/features/git/CommitFeature.java
@@ -0,0 +1,16 @@
+package fr.epita.assistants.features.git;
+
+import fr.epita.assistants.myide.domain.entity.Feature;
+import fr.epita.assistants.myide.domain.entity.Project;
+
+public class CommitFeature implements Feature {
+    @Override
+    public ExecutionReport execute(Project project, Object... params) {
+        return null;
+    }
+
+    @Override
+    public Type type() {
+        return null;
+    }
+}
