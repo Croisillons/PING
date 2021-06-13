091985567583bb35d02d91d3d4f66cfde823cc72
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2021-06-13 18:56:21 +0200

    fix testsuite

--- src/test/java/fr/epita/assistants/features/maven/TreeFeatureTest.java
+++ src/test/java/fr/epita/assistants/features/maven/TreeFeatureTest.java
@@ -4,11 +4,13 @@ import fr.epita.assistants.myide.domain.entity.Feature;
 import fr.epita.assistants.myide.domain.entity.Mandatory;
 import fr.epita.assistants.myide.domain.entity.Project;
 import fr.epita.assistants.myide.domain.service.MyProjectService;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Optional;
 
@@ -20,14 +22,34 @@ class TreeFeatureTest {
     private MyProjectService projectService;
 
     @BeforeEach
-    void setUp()
-    {
+    void setUp() {
         projectService = new MyProjectService();
+        file = new File("src/test/testFiles/tmp");
+        path = file.toPath();
+        file.mkdirs();
     }
 
+    @AfterEach
+    void tearDown() {
+        deleteDir(file);
+    }
+
+    public void deleteDir(File dir) {
+        File[] files = dir.listFiles();
+        if (files != null) {
+            for (final File file : files)
+                deleteDir(file);
+        }
+        dir.delete();
+    }
+
+
     @Test
-    public void tree() {
-        Project project = projectService.load(Path.of("."));
+    public void tree() throws IOException
+    {
+        File pom = new File("src/test/testFiles/tmp/pom.xml");
+        pom.createNewFile();
+        Project project = projectService.load(path);
         Optional<Feature> feature = project.getFeature(Mandatory.Features.Maven.TREE);
         Assertions.assertTrue(feature.isPresent());
 
