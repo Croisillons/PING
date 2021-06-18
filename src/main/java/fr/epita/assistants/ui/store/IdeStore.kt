8c8808455206369f2b710a22a38e47d479a88ef6
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2021-06-18 10:48:20 +0200

    fix (warnings)

--- src/main/java/fr/epita/assistants/ui/store/IdeStore.kt
+++ src/main/java/fr/epita/assistants/ui/store/IdeStore.kt
@@ -2,16 +2,9 @@ package fr.epita.assistants.ui.store
 
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import fr.epita.assistants.myide.domain.entity.Feature
 import fr.epita.assistants.myide.domain.entity.Mandatory
 import fr.epita.assistants.myide.domain.service.MyProjectService
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import java.io.File
 import java.nio.file.Path
-import javax.sound.sampled.AudioSystem
 import javax.swing.JFileChooser
 import javax.swing.JPanel
 
@@ -38,12 +31,12 @@ class IdeStore(val projectService: MyProjectService, val setting: SettingStore) 
      * Load a project from a path
      * @param path Path to the project root
      */
-    fun loadProject(path: String) {
+    private fun loadProject(path: String) {
         project.value = ProjectStore(this, projectService.load(Path.of(path)))
     }
 
     /**
-     * Clean a project, deleteting all files specified in .myideignore
+     * Clean a project, deleting all files specified in .myideignore
      */
     fun cleanProject() {
         project.value?.let {
