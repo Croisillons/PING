27e756ba3ee7df8c3c299feda3c9ac0e2e8f876e
Tony HENG <tony.heng@epita.fr>
2021-07-05 21:04:22 +0200

    update (doc): model package

--- src/main/java/fr/epita/assistants/ui/model/Shortcut.kt
+++ src/main/java/fr/epita/assistants/ui/model/Shortcut.kt
@@ -2,14 +2,26 @@ package fr.epita.assistants.ui.model
 
 import androidx.compose.ui.input.key.*
 
+/**
+ * A shortcut is composed of a Key and at least one of the three: ctrl/shift/alt
+ */
 class Shortcut(val ctrl: Boolean = false, val shift: Boolean = false, val alt: Boolean = false, val key: Key) {
-    fun isPressed(event: KeyEvent) : Boolean {
+    /**
+     * Checks if the KeyEvent corresponds to the shortcut
+     * @param event: the KeyEvent
+     * @return is the keyEvent corresponds to the shortcut
+     */
+    fun isPressed(event: KeyEvent): Boolean {
         return event.isCtrlPressed == ctrl
                 && event.isShiftPressed == shift
                 && event.isAltPressed == alt
                 && event.key == key
     }
 
+    /**
+     * Override toString to display the key combination
+     * @return the shortcut as a string
+     */
     override fun toString(): String {
         var res: String = ""
         if (ctrl) res += "Ctrl + "
